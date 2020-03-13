785eafb44a016fcf4a5bcbd30e0a55a8b6c036cc
Txinto Vaz <txinto@iac.es>
2020-03-13 17:28:48 +0000

    Works with spreadsheets and server.

--- config.py
+++ config.py
@@ -2,7 +2,7 @@
 import os
 
 if (os.name == 'nt'):
-    cte_serial_port = 'COM6:'   # Serial link port for windows machines
+    cte_serial_port = 'COM7:'   # Serial link port for windows machines
 else:
     cte_serial_port = '/dev/ttyUSB0'   # Serial link port for Linux machines
 
