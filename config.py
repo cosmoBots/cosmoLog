75e285943f1123957dc4ad47b805763d4d73096e
Txinto Vaz <txinto@gmail.com>
2020-05-22 12:03:01 +0100

    Added mechanism to automatically link the serial ports based on the USB to serial device information

--- config.py
+++ config.py
@@ -5,7 +5,7 @@ if (os.name == 'nt'):
     cte_serial_port = 'COM6:'   # Serial link port for windows machines
     cte_serial_port2 = 'COM5:'
 else:
-    cte_serial_port = '/dev/ttyUSB0'   # Serial link port for Linux machines    
-    cte_serial_port2 = '/dev/ttyUSB1'   # Serial link port for Linux machines
+    cte_serial_port = '/dev/pfeiUSB'   # Serial link port for Linux machines    
+    cte_serial_port2 = '/dev/lakeUSB'   # Serial link port for Linux machines
 
 cte_verbose = True
